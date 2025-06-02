7ceb7f286bf61ca6b219099fb180500a5d858910
Khaeshi <kagtabss@gmail.com>
2025-06-02 18:03:14 +0800

    added font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'system-ui', 'sans-serif'],
+      },
       colors: {
         gray: {
           50: '#f9fafb',
