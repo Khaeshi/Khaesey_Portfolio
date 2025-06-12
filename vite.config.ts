54e6cbd16df2a1f6e2c93dc8ba958818a6d61084
Khaeshi <kagtabss@gmail.com>
2025-06-12 21:24:21 +0800

    github dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base: 'https://api.github.com/users/Khaeshi/my-resume',
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
