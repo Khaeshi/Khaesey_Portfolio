f87b137663d8fe62b52e33fd77a89f204665a8f2
Khaeshi <kagtabss@gmail.com>
2025-06-14 20:10:42 +0800

    Created spacing between project

--- src/Stack/HomeLayout.vue
+++ src/Stack/HomeLayout.vue
@@ -84,7 +84,7 @@
         <!-- Projects Tab Content -->
         <div
           v-if="activeTab === 'projects'"
-          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4s sm:gap-8"
+          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 sm:gap-8"
         >
           <ScrollTransition
             v-for="(repo, index) in repos"
@@ -100,7 +100,7 @@
               />
               <div>
                 <h3
-                  class="text-xl font-bold heading-font text-neutral-800 dark:text-neutral-100 mb-2"
+                  class="text-xl font-bold heading-font text-neutral-800 dark:text-neutral-100 mb-2 break-words"
                 >
                   {{ repo.name }}
                 </h3>
